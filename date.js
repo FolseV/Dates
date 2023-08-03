e086e11d4f614c4778d407ecfb3d0f5e077092c2
Folse <folsefeeder@gmail.com>
2023-08-04 01:48:10 +0300

    add index.html and date.js

---
+++ date.js
@@ -0,0 +1,38 @@
+const output = document.getElementById("output");
+const fullBtn = document.getElementById("full");
+const dateBtn = document.getElementById("date");
+const timeBtn = document.getElementById("time");
+let mode = "full";
+
+function bindMode(name) {
+  return function () {
+    mode = name;
+    update();
+  };
+}
+
+fullBtn.onclick = bindMode("full");
+dateBtn.onclick = bindMode("date");
+timeBtn.onclick = bindMode("time");
+
+setInterval(update, 1000);
+update();
+
+function update() {
+  output.textContent = format(mode);
+}
+
+function format(formatMode) {
+  const now = new Date();
+
+  switch (formatMode) {
+    case "time":
+      return now.toLocaleTimeString();
+    case "date":
+      return now.toLocaleDateString();
+    case "full":
+      return now.toLocaleDateString() + " " + now.toLocaleTimeString();
+    default:
+      return now.toLocaleTimeString();
+  }
+}
